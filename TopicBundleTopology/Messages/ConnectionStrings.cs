2ae15ead2b0d19d16ec8ca3a51eea01b2a071ebb
Yves Goeleven <yves@goeleven.com>
2014-12-05 14:29:47 +0100

    topic bundle topology

---
+++ TopicBundleTopology/Messages/ConnectionStrings.cs
@@ -0,0 +1,8 @@
+﻿namespace Messages
+{
+    public static class ConnectionStrings
+    {
+        public const string PrimaryNamespace = "Endpoint=sb://topobybundle1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=F+2isy9V5X2gzwy01NL261ljAIxF7BJ2PSi0518AkI4=";
+        public const string SecondaryNamespace = "Endpoint=sb://topobybundle2.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=V1RuMaV3H08MfvXkPEcKWoUhVOfmuc806Mfign74a7M=";
+    }
+}
